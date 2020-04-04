37836fcc805207d9a46da1df9e30a10d679d969b
Vishal Bhadade <vishal.bhadade@gmail.com>
2020-04-04 13:34:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 """The python wrapper for Binary Option API package setup."""
-from setuptools import (setup, find_packages)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+import setuptools
 
-setup(
+setuptools.setup(
     name="binarybotapi",
     version="1.0.0",
-    packages=find_packages(),
-    install_requires=["pylint","requests","websocket-client"],
-    include_package_data = True,
-    description="Binary Option API for IQOption in python",
-    long_description="Binary Option API for IQOption in python",
-    url="https://github.com/vish4mi/binarybotapi",
     author="Vishal Bhadade",
     author_email="vishal.bhadade@gmail.com",
+    description="Binary Option API for IQOption in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vish4mi/binarybotapi",
+    packages=setuptools.find_packages(),
+    install_requires=["pylint","requests","websocket-client"],
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
     zip_safe=False
 )
